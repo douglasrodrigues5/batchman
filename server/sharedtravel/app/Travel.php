7a09e317e52af9413b18a1765a4a561ba78292c9
douglasrodrigues5 <douglasrodriguesguima@gmail.com>
2017-10-28 12:53:32 -0200

    Travels model and migration

---
+++ server/sharedtravel/app/Travel.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Travel extends Model{
+    //The table we are current working on.
+    public $table = "travels";
+
+    //All fields are required for any HTTP requisition. 
+    protected $fillable = [
+        'start_location', //string
+        'end_location', //tring
+        'driver_id' //integer(driver_id foreign key)
+    ];
+
+    //Automatic fields.
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ]; 
+
+
+}
+
+?>
